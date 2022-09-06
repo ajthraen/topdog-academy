3bec155b14f204116a6724d58c48379d21a78eab
AJ <ajthraen@gmail.com>
2022-09-05 20:55:23 -0400

    faq done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
+let faqButton = document.querySelector(".faqButton");
+let faqButton2 = document.querySelector(".faqButton2");
+let faqButton3 = document.querySelector(".faqButton3");
 
 btn.onclick = function() {
     sidebar.classList.toggle("active");
@@ -8,12 +11,15 @@ btn.onclick = function() {
 function myFunction() {
     var element = document.getElementById("myDIV");
     element.classList.toggle("active");
+    faqButton.classList.toggle("active");
 }
 function myFunction2() {
     var element = document.getElementById("myDIV2");
     element.classList.toggle("active");
+    faqButton2.classList.toggle("active")
 }
 function myFunction3() {
     var element = document.getElementById("myDIV3");
     element.classList.toggle("active");
+    faqButton3.classList.toggle("active")
 }
